a582975e3d99b7afd4e96a7d4735a47945a6f51b
Juha Liias <juha.liias@gmail.com>
2021-02-03 22:23:08 +0200

    Add project files.

---
+++ VendingConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachineLibrary;
+
+namespace VendingConsole
+{
+    class Program
+    {
+        enum ItemType { food, drink, weapon };
+
+        static void Main(string[] args)
+        {
+            List<Item> testItems = new List<Item>();
+
+            VendingMachine myMachine = new VendingMachine();
+
+            Item bread = new Item("bread", 5, "food");
+
+            myMachine.AddItem(bread);
+
+            testItems = myMachine.ListItems();
+
+            foreach (Item i in testItems) {
+                Console.WriteLine(i.GetName() + " : " + i.GetPrice());
+            }
+
+        }
+    }
+}
---
+++ VendingMachineLibrary/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineLibrary
+{
+    enum ItemType { food, drink, weapon };
+
+    public class VendingMachine
+    {
+        // list for items that are loaded to vending machine
+        private List<Item> itemList;
+
+        public VendingMachine()
+        {
+            this.itemList = new List<Item>();
+        }
+
+        public void AddItem(Item newItem)
+        {
+            itemList.Add(newItem);
+        }
+
+        public List<Item> ListItems()
+        {
+            if (itemList.Count > 0)
+            {
+                return itemList;
+            }
+
+            return null;
+        }
+    }
+
+    public class Item
+    {
+        private string name;
+        private int price;
+        public string itemType;
+
+        public Item(string name, int price, string itemType)
+        {
+            this.name = name;
+            this.price = price;
+            this.itemType = itemType;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public int GetPrice()
+        {
+            return this.price;
+        }
+    }
+}
